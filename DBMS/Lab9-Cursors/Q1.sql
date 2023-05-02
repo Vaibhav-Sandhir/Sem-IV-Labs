3ca89e888c342639f68526d970f61b3a95a39472
Vaibhav Sandhir <113308104+Vaibhav-Sandhir@users.noreply.github.com>
2023-05-02 17:27:37 +0530

    Update Q1.sql

--- DBMS/Lab9-Cursors/Q1.sql
+++ DBMS/Lab9-Cursors/Q1.sql
@@ -9,22 +9,18 @@ CREATE TABLE salary_raise (
 SET SERVEROUTPUT ON
 
 DECLARE 
-    CURSOR C(dname instructor.dept_name%TYPE) IS SELECT * FROM instructor WHERE dept_name = dname;
+    CURSOR C(dname instructor.dept_name%TYPE) IS SELECT * FROM instructor WHERE dept_name = dname FOR UPDATE;
     pl_dept_name instructor.dept_name%TYPE;
-    inst_row instructor%ROWTYPE;
 
 BEGIN 
     pl_dept_name := '&Dept_Name';
-    OPEN C(pl_dept_name);
+    FOR i IN C(pl_dept_name) 
     LOOP
-        FETCH C INTO inst_row;
-        EXIT WHEN C%NOTFOUND;
-        UPDATE instructor SET salary = salary * 1.05
-        WHERE ID = inst_row.ID;
-        INSERT INTO salary_raise VALUES(inst_row.ID,SYSDATE,inst_row.salary * 0.05);
-    END LOOP;
-    CLOSE C;
+        UPDATE instructor SET salary = salary * 1.05 WHERE CURRENT OF C;
+        INSERT INTO salary_raise VALUES(i.id,SYSDATE,i.salary * 0.05);
+    END LOOP;            
 END;
 /   
 
 SELECT * FROM salary_raise;
+
