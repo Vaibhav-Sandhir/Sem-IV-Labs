b9d95ce2b97ad3360487736b6662b10d6add351b
Vaibhav Sandhir <113308104+Vaibhav-Sandhir@users.noreply.github.com>
2023-05-02 17:40:32 +0530

    Update Q2.sql

--- DBMS/Lab9-Cursors/Q2.sql
+++ DBMS/Lab9-Cursors/Q2.sql
@@ -1,18 +1,17 @@
 SET SERVEROUTPUT ON
 
-DECLARE 
-    CURSOR C IS SELECT * FROM student ORDER BY tot_cred ASC;
-    stu student%ROWTYPE;
-    i NUMBER(2) := 1;
+DECLARE
+    CURSOR C IS SELECT* FROM student ORDER BY tot_cred ASC;
+    i NUMERIC(2) := 1;
 
 BEGIN
-    OPEN C;
-    WHILE i <= 10 LOOP  
-        FETCH C INTO stu;
-        DBMS_OUTPUT.PUT_LINE('ID: ' || stu.ID);
-        DBMS_OUTPUT.PUT_LINE('Credits: ' || stu.tot_cred);
+    FOR j IN C
+    LOOP
+        DBMS_OUTPUT.PUT_LINE(j.id || ' ' ||  j.name  || ' ' || j.dept_name || ' ' || j.tot_cred);
         i := i + 1;
+        IF i > 10 THEN
+            EXIT;
+        END IF;
     END LOOP;
-    CLOSE C;
 END;
-/                
+/                  
